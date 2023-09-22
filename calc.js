fe6ed932108d22c8cb3729b57cbd7a02b8e665db
Arjun Kurup <91071122+thekurup@users.noreply.github.com>
2023-09-22 20:05:59 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,12 @@
+// function press(x){
+//   document.getElementById("screen").value=document.getElementById("screen").value+x;
+// }
+// function cl(){
+//   document.getElementById("screen").value="";
+
+// }
+// function equalcl(){
+//   var text = document.getElementById("screen").value;
+//   var result = eval(text);
+//   document.getElementById("screen").value=result
+// }
